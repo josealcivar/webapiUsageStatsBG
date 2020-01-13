6cf712803ad8063919b56a5af9338fafc8032cd4
JoseAndre <JoseAndre@DESKTOP-CFGHQSK.(none)>
2020-01-13 01:46:47 -0500

    correciones del api

--- controller/usagestats.controller.js
+++ controller/usagestats.controller.js
@@ -66,13 +66,13 @@ let t = await inicializarTransaccion();
         };
         console.log("MOVIMIENTOS");
         console.log(movimiento);
-        
-       let is_saved = await modelo.Movimientosapp.ActualizaMovimiento(movimiento, t);
-       console.log(is_saved);
-        if(is_saved[0]==0){
-            console.log("guarda el movimiento");
-            await modelo.Movimientosapp.CrearMovimientos(movimiento, t);
-        }
+        await modelo.Movimientosapp.CrearMovimientos(movimiento, t);
+    //    let is_saved = await modelo.Movimientosapp.ActualizaMovimiento(movimiento, t);
+    //    console.log(is_saved);
+    //     if(is_saved[0]==0){
+    //         console.log("guarda el movimiento");
+    //         await modelo.Movimientosapp.CrearMovimientos(movimiento, t);
+    //     }
       //  
     }
     //console.log(count)
