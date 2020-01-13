ca3a7c16f16432683782ed3e3efe05487e2b11fc
JoseAndre <JoseAndre@DESKTOP-CFGHQSK.(none)>
2020-01-13 02:09:57 -0500

    correcion

--- config/config.js
+++ config/config.js
@@ -56,13 +56,7 @@ module.exports =
     "host": "us-cdbr-iron-east-05.cleardb.net",
     "dialect": "mysql",
     "operatorsAliases":operadores,
-    "logging": false, 
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    "logging": false
   },
   "test": {
     "username": "root",
